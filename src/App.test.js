66bf32778d15fe5e3a0badbe0b31e5c8015a5ec8
parkminah123 <hello.git.kna9919@gmail.com>
2024-06-20 02:26:07 +0900

    yml

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
+import "@testing-library/jest-dom"; // 확장 임포트
 import App from "./App";
-import "./pure";
 
 test("renders learn react link", () => {
   const { getByText } = render(<App />);
