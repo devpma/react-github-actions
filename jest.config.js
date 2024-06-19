f622b7e996a59bb080c1567ff9d443188771e1ea
parkminah123 <hello.git.kna9919@gmail.com>
2024-06-19 21:13:03 +0900

    yml

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  testEnvironment: "jest-environment-jsdom",
+  testEnvironment: require.resolve("jest-environment-jsdom"),
   moduleNameMapper: {
     "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js",
     "\\.(svg|jpg|jpeg|png)$": "<rootDir>/__mocks__/fileMock.js",
