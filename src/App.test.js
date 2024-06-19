3f8a50a077916045e9dc0f5751dcc4a9b6fd21d2
parkminah123 <hello.git.kna9919@gmail.com>
2024-06-20 01:33:52 +0900

    Update GitHub Actions workflow and package.json

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import "@testing-library/jest-dom"; // 확장 임포트
+import "@testing-library/jest-dom/extend-expect";
 import App from "./App";
 
 test("renders learn react link", () => {
