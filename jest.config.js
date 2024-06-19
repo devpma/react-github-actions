0ad9bee13b3361061351a2b4c4ce8d7e218984a4
parkminah123 <hello.git.kna9919@gmail.com>
2024-06-19 20:12:57 +0900

    yml

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
     "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js",
     "\\.(svg|jpg|jpeg|png)$": "<rootDir>/__mocks__/fileMock.js",
   },
+  moduleDirectories: ["node_modules", "<rootDir>/src"],
   transform: {
     "^.+\\.jsx?$": "babel-jest",
   },
--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import "@testing-library/jest-dom"; // 확장 임포트
 import App from "./App";
 
 test("renders learn react link", () => {
