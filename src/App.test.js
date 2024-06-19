6325b423893d914a2e8e31456664994796790890
parkminah123 <hello.git.kna9919@gmail.com>
2024-06-20 02:05:49 +0900

    npm install

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import App from "./App";
+import "./pure";
 
 test("renders learn react link", () => {
   const { getByText } = render(<App />);
